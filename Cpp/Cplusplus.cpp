678608933f41a55c8b72aad3350b0f93d67abef4
Amirmohammad Kazemi <78997499+Amirmohammadkazemi@users.noreply.github.com>
2021-02-19 20:42:11 +0330

    Further Details

--- Cpp/Cplusplus.cpp
+++ Cpp/Cplusplus.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!";
+int main(){
+    cout << "Hello World!"; //You can write std::cout<<"hello word!" 
     return 0;
 }
